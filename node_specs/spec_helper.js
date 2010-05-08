c0d051a1df979907f03efe0b57e8e648f2b5a03d
Brian Takita <brian@honk.com>
2010-05-08 11:18:28 -0700

    Created DescribeHelper. Fixed specs.

--- node_specs/spec_helper.js
+++ node_specs/spec_helper.js
@@ -1,6 +1,7 @@
 var path = require("path"),
   sys = require("sys"),
-  childProcess = require("child_process");
+  childProcess = require("child_process"),
+  http = require('http');
 var jsTestServerDir = path.join(path.dirname(__filename), "../lib");
 require.paths.push(jsTestServerDir);
 
@@ -17,7 +18,43 @@ var SpecHelper = {
     return path.join(this.libRoot(), "js_test_server/app.js")
   },
   startServer: function() {
-    return childProcess.spawn("node", [this.serverRoot()]);
+    var proc = childProcess.spawn("node", [this.serverRoot()]);
+    var dataListener = function(data) {
+      if (/Express started at /.exec(data)) {
+        serverStarted = true;
+      }
+    };
+    proc.stdout.addListener("data", dataListener);
+
+    waitsFor(10000, function() {
+      try {
+        return serverStarted &&
+          http.createClient(
+            process.env['JS_TEST_SERVER_PORT'], "localhost"
+          ).request("GET", "/", {"host": "localhost"});
+      } catch(e) {
+        return false;
+      }
+    });
+    return proc;
   }
 }
 global.SpecHelper = SpecHelper;
+
+var DescribeHelper = {
+  useServer: function(describe) {
+    var proc;
+    describe.beforeEach(function() {
+      process.env['JS_TEST_SERVER_PORT'] = 3000;
+
+      var serverStarted = false;
+      proc = SpecHelper.startServer();
+    });
+
+    describe.afterEach(function() {
+      proc.removeAllListeners();
+      proc.kill();
+    });
+  }
+}
+global.DescribeHelper = DescribeHelper;
