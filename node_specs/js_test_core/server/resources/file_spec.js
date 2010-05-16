5f7504fdb131eb90058cf432e5d7f6cf560f64e7
Brian Takita <brian@honk.com>
2010-05-15 17:10:54 -0700

    Fixed rendering directory file links.

--- node_specs/js_test_core/server/resources/file_spec.js
+++ node_specs/js_test_core/server/resources/file_spec.js
@@ -9,12 +9,12 @@ describe("GET /javascripts/foo.js - a file", function() {
   });
 });
 
-//describe("GET /javascripts - a directory", function() {
-//  it("renders the list of files in the directory with the give path appended to --root-path", function() {
-//    SpecHelper.performRequest("GET", "/javascripts", {"host": "localhost"}, function(body) {
-//      expect(body).toMatch("/javascripts/foo.js")
-//      expect(body).toMatch("/javascripts/large_file.js")
-//      expect(body).toMatch("/javascripts/subdir")
-//    });
-//  });
-//});
+describe("GET /javascripts - a directory", function() {
+  it("renders the list of files in the directory with the give path appended to --root-path", function() {
+    SpecHelper.performRequest("GET", "/javascripts", {"host": "localhost"}, function(body) {
+      expect(body).toMatch("/javascripts/foo.js")
+      expect(body).toMatch("/javascripts/large_file.js")
+      expect(body).toMatch("/javascripts/subdir")
+    });
+  });
+});
