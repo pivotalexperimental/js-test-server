c5517d161f5e3f1b25c95c8594c1bd8f19f340c4
Brian Takita <brian@honk.com>
2010-05-23 01:47:04 -0700

    Fixed loading of xmlbuilder.js Added shell for spec_file.js

---
+++ node_specs/js_test_core/server/resources/spec_file_spec.js
@@ -0,0 +1,15 @@
+var sys = require('sys'),
+    http = require('http');
+
+describe("GET /specs/passing_spec.js - a file", function() {
+  it("renders project js files", function() {
+//    doc.at("script[@src='/javascripts/test_file_1.js']").should_not be_nil
+//    doc.at("script[@src='/javascripts/test_file_2.js']").should_not be_nil
+  })
+
+  it("renders a file the given path appended to --root-path", function() {
+//    SpecHelper.performRequest("GET", "/javascripts/foo.js", {"host": "localhost"}, function(response) {
+//      expect(response.body).toMatch("Foo")
+//    });
+  });
+});
