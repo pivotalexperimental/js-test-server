e94bdd078306f11c389199dac75bcddfb38cbbf4
Brian Takita <brian@honk.com>
2010-05-31 02:10:26 -0700

    Added spec_file resource.

--- node_specs/js_test_core/server/resources/spec_file_spec.js
+++ node_specs/js_test_core/server/resources/spec_file_spec.js
@@ -1,10 +1,13 @@
 var sys = require('sys'),
-    http = require('http');
+    http = require('http')
 
 describe("GET /specs/passing_spec.js - a file", function() {
   it("renders project js files", function() {
-//    doc.at("script[@src='/javascripts/test_file_1.js']").should_not be_nil
-//    doc.at("script[@src='/javascripts/test_file_2.js']").should_not be_nil
+    SpecHelper.performRequest("GET", "/specs/passing_spec", {"host": "localhost"}, function(response) {
+      require('sys').puts(response.body)
+      //    expect(Y.one("script[@src='/javascripts/test_file_1.js']")).toNotBeNull()
+      //    expect(Y.one("script[@src='/javascripts/test_file_2.js']")).toNotBeNull()
+    });
   })
 
   it("renders a file the given path appended to --root-path", function() {
